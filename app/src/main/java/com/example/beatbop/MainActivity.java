9a6f39cb6ac7faf80f61386c74d09caa729fab13
DoniVerse <arsemateferi5@gmail.com>
2025-05-18 14:27:25 +0300

    first

---
+++ app/src/main/java/com/example/beatbop/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.beatbop;
+
+
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button signupButton;
+    TextView loginLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signupButton = findViewById(R.id.signupButton);
+        loginLink = findViewById(R.id.loginButton);
+
+        signupButton.setOnClickListener(v ->
+                Toast.makeText(MainActivity.this, "Sign-up clicked", Toast.LENGTH_SHORT).show()
+        );
+
+        loginLink.setOnClickListener(v ->
+                Toast.makeText(MainActivity.this, "Login clicked", Toast.LENGTH_SHORT).show()
+        );
+    }
+}
